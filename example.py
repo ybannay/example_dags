31512e691b02e86207ac6e0e893b7bef942f4295
ybannay <youssefbannay@gmail.com>
2019-06-30 14:57:39 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""
 from airflow import DAG
+from airflow.operators import BashOperator
 from datetime import datetime, timedelta
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.operators.dummy_operator import DummyOperator
-
-
+from airflow.operators.docker_operator import DockerOperator
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -14,34 +31,29 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
 }
-
 dag = DAG(
-    'kubernetes_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
-
-
-start = DummyOperator(task_id='run_this_first', dag=dag)
-
-passing = KubernetesPodOperator(namespace='default',
-                          image="Python:3.6",
-                          cmds=["Python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="passing-test",
-                          task_id="passing-task",
-                          get_logs=True,
-                          dag=dag
-                          )
-
-failing = KubernetesPodOperator(namespace='default',
-                          image="ubuntu:1604",
-                          cmds=["Python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="fail",
-                          task_id="failing-task",
-                          get_logs=True,
-                          dag=dag
-                          )
-
-passing.set_upstream(start)
-failing.set_upstream(start)
+    'docker_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag)
+t2 = BashOperator(
+    task_id='sleep',
+    bash_command='sleep 5',
+    retries=3,
+    dag=dag)
+t3 = DockerOperator(api_version='1.19',
+    docker_url='tcp://localhost:2375', #Set your docker URL
+    command='/bin/sleep 30',
+    image='centos:latest',
+    network_mode='bridge',
+    task_id='docker_op_tester',
+    dag=dag)
+t4 = BashOperator(
+    task_id='print_hello',
+    bash_command='echo "hello world!!!"',
+    dag=dag)
+t1.set_downstream(t2)
+t1.set_downstream(t3)
+t3.set_downstream(t4)
+"""
